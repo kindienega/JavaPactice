07c6334faca0064a36e2dc0b4471488e21bf3a44
Kindie <kindienega10@gmail.com>
2024-02-21 23:20:21 +0300

    for loop

--- src/com/kindie/Main.java
+++ src/com/kindie/Main.java
@@ -1,7 +1,20 @@
 package com.kindie;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++){
+            System.out.println("hello");
+        }
+        String[] names = {"alex","alula","aleeeex","alfeeeula","alfkkfkfkkfex","aluffffla","aledcfddedx","alusddla"} ;
+        for (int i = 0; i<names.length; i++){
+            System.out.println(names[i] + " -------------> ass its index "+  i);
+        }
+        for(String name : names){
+            System.out.println(name);
+        }
+
         int age = 40;
         int ageCopy = increment(age);
         System.out.println(age);
---
+++ src/com/kindie/loops.java
@@ -0,0 +1,11 @@
+package com.kindie;
+
+public class loops {
+//    class print() {
+//    for(int i = 0;i<=10;i++)
+//
+//        {
+//            System.out.println(i);
+//        }
+//    }
+}
